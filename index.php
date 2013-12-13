1519c74fe6d5942e4f508eb130b0b433eaa3133d
antoniopuero <antonio.puero@gmail.com>
2013-12-13 22:42:03 +0200

    changes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include('./config.php'); ?>
-<?php session_start(); ?>
+<?php session_start();
+        echo session_name(); ?>
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
