74a2af90675294e6b855b8eefc5a8287e3f32f33
antoniopuero <antonio.puero@gmail.com>
2013-12-13 22:35:36 +0200

    changes

--- confirm.php
+++ confirm.php
@@ -1,6 +1,6 @@
 <?php
 if(count($_POST)>0){
-    if(isset($_SESSION['captcha_keystring']) && $_SESSION['captcha_keystring'] === $_POST['keystring']){
+    if(isset($_SESSION['captcha_keystring']) && $_SESSION['captcha_keystring'] === $_POST['captcha']){
         echo "Correct";
     }else{
         echo "Wrong";
