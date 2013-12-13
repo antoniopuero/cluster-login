fd4cfb40dd1ee2c6c7ac21764f31034f1b8b8e81
antoniopuero <antonio.puero@gmail.com>
2013-12-13 22:57:56 +0200

    changes

--- confirm.php
+++ confirm.php
@@ -1,11 +1,22 @@
+<?php print_r($_REQUEST); ?>
 <?php session_start(); ?>
 <?php
 if(count($_POST)>0){
     if(isset($_SESSION['captcha_keystring']) && $_SESSION['captcha_keystring'] === $_POST['captcha']){
         echo "Correct";
-    }else{
-        echo "Wrong";
-    }
+    } else { ?>
+        <form action='Page_C.php' method='post' name='frm'>
+        <?php
+        foreach ($_POST as $a => $b) {
+            echo "<input type='hidden' name='".htmlentities($a)."' value='".htmlentities($b)."'>";
+        }
+        ?>
+        </form>
+        <script>
+            document.frm.submit();
+        </script>
+
+    <?php }
 }
 unset($_SESSION['captcha_keystring']);
 ?>
