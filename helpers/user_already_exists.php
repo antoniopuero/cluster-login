e5bc0d200af984072c8936d38c94820dc9e07ec1
antoniopuero <antonio.puero@gmail.com>
2013-12-22 19:38:27 +0200

    changes

--- helpers/user_already_exists.php
+++ helpers/user_already_exists.php
@@ -6,8 +6,10 @@
  * Time: 11:38 AM
  * To change this template use File | Settings | File Templates.
  */
-include('./config.php');
-include($helpers . 'read_directory.php');
+if (!function_exists(files_in_directory)) {
+    include('./config.php');
+    include($helpers . 'read_directory.php');
+}
 function user_exists($login, $pre_query_dir, $query_dir)
 {
     $files = files_in_directory($pre_query_dir);
