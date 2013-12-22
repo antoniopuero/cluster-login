86ad2f22425dc0ec66ccf0cf194e218b2927f467
antoniopuero <antonio.puero@gmail.com>
2013-12-22 19:16:04 +0200

    changes

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ $path_to_confirm = "./confirm.php";
 $path_to_captcha = "./kcaptcha/index.php";
 $helpers = "./helpers/";
 $pre_query_folder = "./pre-query/";
-$query_folder = ".users-query/";
+$query_folder = "./users-query/";
 ?>
