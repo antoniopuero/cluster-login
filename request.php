08278c2737e418b127b553a65ed8ca8dac6a6688
antoniopuero <antonio.puero@gmail.com>
2013-12-23 22:51:31 +0200

    changes

--- request.php
+++ request.php
@@ -1,3 +1,31 @@
 <?php
-print_r($_POST);
-?>
+extract($_GET);
+include('./config.php');
+$filename = $login . "+" . $date . ".json";
+$path_pre_query = $pre_query_folder . $filename;
+$path_query = $query_folder . $filename;
+if (file_exists($path_pre_query)) {
+    rename($path_pre_query, $path_query);
+    ?>
+    <html>
+    <head>
+        <link rel="stylesheet" src="./style.css">
+        <title>Підтверджено</title>
+    </head>
+    <body>
+    Дякуємо за підтвердження реєстрації. Чекайте на лист адміністратора з подальшими інструкціями.
+    </body>
+    </html>
+<?php } else if (file_exists($path_query)) { ?>
+    <html>
+    <head>
+        <link rel="stylesheet" src="./style.css">
+        <title>Підтверджено</title>
+    </head>
+    <body>
+    Ваш профіль вже підтверджено. Чекайте, будь-ласка, на лист від адміністратора.
+    </body>
+    </html>
+<?php } else {
+
+} ?>
