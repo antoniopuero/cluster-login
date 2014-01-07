1b46826fd7434864d9eb8f8a87dcf1371448fa19
antoniopuero <antonio.puero@gmail.com>
2014-01-07 19:04:14 +0200

    uncomment ldap using

--- helpers/send_email_to_user.php
+++ helpers/send_email_to_user.php
@@ -1,4 +1,5 @@
 <?php
+	include('../config.php');
 	function send_email_to_user ($email, $login, $username, $passwd) {
 		$body = "Dear " . $username . "!
 			Your application for user account registration at IFBG cluster has been approved.
