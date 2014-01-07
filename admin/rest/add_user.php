a26e981ada5f71c58ec5b2cce6d96d6f2b20d244
antoniopuero <antonio.puero@gmail.com>
2014-01-07 20:47:59 +0200

    debug && changes

--- admin/rest/add_user.php
+++ admin/rest/add_user.php
@@ -18,12 +18,11 @@ $connection = ldap_connect($ldaphost, $ldapport);
 		if ($ldap_bind) {
 			$info = ldap_search($connection, $_SESSION['ldaprdn'], "(cn=*)");
 			$gids = ldap_get_entries($connection, $info);
-			print_r($info);
 			print_r($gids);
 			for ($i = 0, $max = 1001; $i < $gids['count']; $i++) {
 				if (isset($gids[$i]['gid'])) {
-					$temp = $gids[$i]['gid'];
-					if (($temp < 5000) && ($temp > $max)) {
+					$temp = $gids[$i]['gid'][0];
+					if (isset($temp) && ($temp < 5000) && ($temp > $max)) {
 						$max = $temp;
 					}
 				}
